5792c5a2b7b1fa6fce53296324e4ab2982fc71d9
Ayushi Patel <ashapatel99246@gmail.com>
2025-04-25 15:03:55 +0530

    city controller upd

--- src/controllers/cityController.js
+++ src/controllers/cityController.js
@@ -1,50 +1,60 @@
-const City = require('../models/cityModel')
+const City = require('../models/cityModel');
 
-//add city
+
+// Add City
 const addCity = async (req, res) => {
-    try {
-
-        const savedCity = await City.create(req.body);
-        res.status(201).json({
-            message: "City added successfully",
-            data: savedCity
-        })
-    } catch (err) {
-        res.status(500).json({ message: err });
-    }
+  try {
+    const savedCity = await City.create(req.body);
+    res.status(201).json({
+      message: "City added successfully",
+      data: savedCity
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
-//get city
+// Get All Cities
 const getCities = async (req, res) => {
-    try {
-        const cities = await City.find().populate("stateId");
-        res.status(200).json({
-            message: "All cities",
-            data: cities,
-        });
-    } catch (err) {
-        res.status(500).json({ message: err });
-    }
+  try {
+    const cities = await City.find().populate("stateId");
+    res.status(200).json({
+      message: "All cities",
+      data: cities,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
-
+// Get Cities by State ID
 const getCityByStateId = async (req, res) => {
-    const stateId = req.params.stateId;
-    try {
-        const cities = await City.find({ stateId: req.params.stateId })
-        res.status(200).json({
-            message:"city found",
-            data:cities,
-        })
-    } catch (err) {
-        res.status(200).json({
-            message:"city not found"
-        })
+  const stateId = req.params.stateId;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(stateId)) {
+      return res.status(400).json({ message: "Invalid state ID" });
     }
-}
+
+    const cities = await City.find({ stateId: new mongoose.Types.ObjectId(stateId) });
+
+    if (cities.length === 0) {
+      return res.status(200).json({ message: "No cities found", data: [] });
+    }
+
+    res.status(200).json({
+      message: "city found",
+      data: cities,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Error fetching cities",
+      error: err.message,
+    });
+  }
+};
 
 module.exports = {
-    addCity,
-    getCities,
-    getCityByStateId,
+  addCity,
+  getCities,
+  getCityByStateId,
 };
